e6b9507b8fd75cb87a61c01780c537a0867bd45c
aching <aching@73f437aa-624d-0410-a3ad-9285e900ec26>
2010-12-08 22:49:46 +0000

    [Bug 4177788] Convert project.mk to use unittests.  Fixed result.xml to be put into the top_build directory.
    git-svn-id: svn+ssh://svn.corp.yahoo.com/yahoo/yst/websearch/projects/clusterlib/trunk@272177 73f437aa-624d-0410-a3ad-9285e900ec26

--- project.mk
+++ project.mk
@@ -28,7 +28,6 @@ export CXXFLAGS=-static
 export CPPFLAGS=-I/usr/releng/external/boost/1.39.0_gcc411/include/boost-1_39 -I/usr/releng/internal/zookeeper/zookeeper-1.0.0/include -I/usr/releng/external/log4cxx/0.10.0_gcc411/include -I. -I/usr/releng/external/libmicrohttpd/0.4.0_gcc411/include -I/usr/releng/external/apr/1.2.12/include/apr-1 -I/usr/releng/external/apr-util/1.2.12/include/apr-1 -I/usr/releng/external/mpich2/mpich2-latest/install/include -I/usr/releng/external/cppunit/1.12.0_gcc411/include
 export LDFLAGS=-L/usr/releng/external/boost/1.39.0_gcc411/lib -L/usr/releng/internal/zookeeper/zookeeper-1.0.0/lib -L/usr/releng/external/log4cxx/0.10.0_gcc411/lib -L/usr/releng/external/libmicrohttpd/0.4.0_gcc411/lib -L/usr/releng/external/apr/1.2.12/lib -L/usr/releng/external/apr-util/1.2.12/lib -L/usr/releng/external/mpich2/mpich2-latest/install/lib -L/usr/releng/external/cppunit/1.12.0_gcc411/lib -L/usr/local/lib/
 
-
 ############################################################
 
 build_flavor:
@@ -36,12 +35,10 @@ build_flavor:
 	mkdir -p $(BUILD_DIR)
 	cd $(BUILD_DIR) && $(SRC_DIR)/configure $(CONFIGURE_OPTS_$(FLAVOR))
 	$(MAKE) -C $(BUILD_DIR)
+	$(MAKE) -C $(BUILD_DIR) check
 
 package:
 	rm -f $(BUILD_DIR)/*.tar.gz
 	$(MAKE) -C $(BUILD_DIR) tarball
 	new_fn=`ls -1 $(BUILD_DIR)/*.tar.gz | sed -e 's@.tar.gz@.$(ARCH).tar.gz@'`; \
 	mv -f $(BUILD_DIR)/*.tar.gz $$new_fn
-
-test:
-	ulimit -n 1024; $(MAKE) -C $(BUILD_DIR) TAP_PREFIX= check
